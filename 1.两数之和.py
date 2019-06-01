84663de2704670e9a6bbbafb1696849459db483c
liguangjunwill <liguangjunwill@163.com>
2019-06-01 17:26:33 +0800

    two sums python

---
+++ 1.两数之和.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode.cn id=1 lang=python
+#
+# [1] 两数之和
+#
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        map = {}
+        for i in range(len(nums)):
+            if nums[i] not in map:
+                map[target - nums[i]] = i
+            else:
+                return map[nums[i]], i
+
+        return -1, -1
+        
+
